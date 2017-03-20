5cf53fe41b29faa58e0ccf6d6f27cda13a6fcce0
Dishant Munjal <dimunj@microsoft.com>
2017-03-20 11:58:47 +0530

    removing $scope dependency

--- Session/wwwroot/js/App.js
+++ Session/wwwroot/js/App.js
@@ -1,13 +1,17 @@
 ﻿var app = angular.module("app", []);
 
 app.controller("appCtrl", function ($scope) {
-    $scope.pageTitle = "My angular application";
-    $scope.fullName = function (firstName, lastName) {
+    this.pageTitle = "My angular application";
+    this.fullName = function (firstName, lastName) {
         return lastName + ", " + firstName;
     }
-    
 });
-
+app.controller("appCtrl1", function ($scope) {
+    this.pageTitle = "My angular applica";
+    this.fullName = function (firstName, lastName) {
+        return lastName + ", " + firstName;
+    }
+});
 function showDiv() {
     document.getElementById('nameDiv').style.display = "block";
 }
