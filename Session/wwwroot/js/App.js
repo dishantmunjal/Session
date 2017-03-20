5b338ccbae077500262ef70c9525a24fa306c679
Dishant Munjal <dimunj@microsoft.com>
2017-03-20 11:51:22 +0530

    function

--- Session/wwwroot/js/App.js
+++ Session/wwwroot/js/App.js
@@ -2,4 +2,14 @@
 
 app.controller("appCtrl", function ($scope) {
     $scope.pageTitle = "My angular application";
+    $scope.fullName = function (firstName, lastName) {
+        return lastName + ", " + firstName;
+    }
+    
 });
+
+function showDiv() {
+    document.getElementById('nameDiv').style.display = "block";
+}
+
+
